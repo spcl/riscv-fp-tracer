e1836b9c6d1c2af34f9d6cc204449d143a209717
Siyuan Shen <siyuan.shen@inf.ethz.ch>
2024-01-31 12:57:51 +0100

    Wrote README

--- example/square.c
+++ example/square.c
@@ -2,12 +2,14 @@
 #define X 1.2345
 
 // Example code for performing floating point operations
-double __attribute__ ((noinline)) kernel(double num) {
+float __attribute__ ((noinline)) kernel(double num) {
   num += 1.11;
   num *= 1.2;
   for (int i = 0; i < 100; ++i)
-    num += 2.31;
+    num += -2.31;
   num /= 1.5;
+  for (int j = 0; j < 100; ++j)
+    num -= 10;
   return num * num;
 }
 
---
+++ machine-learning/README.md
@@ -0,0 +1,3 @@
+### Machine learning related trace analysis
+
+This directory contains all the code related to machine learning related floating point trace analysis.
